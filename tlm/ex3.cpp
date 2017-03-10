b969bc1bd52d5d3392c4382596b84e0e9010bb6b
Han Cui <hc475@cl.cam.ac.uk>
2017-03-10 15:02:57 +0000

    Added TLM code

---
+++ tlm/ex3.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <systemc.h>
+#include <tlm.h>
+#include <tlm_utils/simple_initiator_socket.h>
+#include "matMul_2x2.h"
+
+
+SC_MODULE(ex3)
+{
+  tlm_utils::simple_initiator_socket<ex3> socket;
+  
+  SC_CTOR(ex3) : socket("socket")
+  {
+    SC_THREAD(run);
+  }
+
+  private:
+  void run() 
+  {
+		tlm::tlm_generic_payload* p = new tlm::tlm_generic_payload();
+		sc_time delay = sc_time(30, SC_NS);
+
+		unsigned char data[16];
+		p->set_data_length(16); 
+		p->set_data_ptr(data);  
+
+		p->set_streaming_width(8);
+		p->set_byte_enable_ptr(0);
+		p->set_dmi_allowed(false);
+		p->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+		 
+		u32* A = (u32*) &data[0];
+		u32* B = (u32*) &data[4];
+		u64* res = (u64*) &data[8];
+
+		sc_core::sc_time d; 
+		*A = 0x12345678; *B = 0x12345678;
+		socket->b_transport(*p, d);  
+		
+		printf("matA = 0x%08x\n", *A);
+		printf("matB = 0x%08x\n", *B);
+		printf("res = 0x%016llx\nmatrix form:\n", *res);
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				printf("%d ", 0xFFFF&((*res)>>(i*2+j)*16));
+			}
+			printf("\n");
+		}	
+	}
+	
+}; 
+int sc_main(int argc, char *argv[])
+{
+  ex3* socex = new ex3("socex");
+  matMul_2x2 device("matMul_2x2");
+  socex->socket.bind(device.port0);
+  sc_start();
+  return 0;
+}
+
+  
---
+++ tlm/matMul_2x2.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <iostream>
+#include <bitset>
+#include "systemc.h"
+#include "tlm_utils/simple_target_socket.h"
+#include "tlm_utils/multi_passthrough_target_socket.h"
+#include "tlm_core/tlm_2/tlm_generic_payload/tlm_gp.h"
+ 
+#define u64 unsigned long long
+#define u16 uint16_t
+#define u8 uint8_t
+#define u32 unsigned int
+
+using namespace std;
+
+/*
+WIDTH of each matrix element = 8 bits
+payload structure (bits):
+	Name		Start			End						Length			
+	A		 		0 			 	4*WIDTH-1			4*WIDTH				4x 8-bit int				
+	B		 		4*WIDTH 	8*WIDTH-1			4*WIDTH				4x 8-bit int
+	Res			8*WIDTH		16*WIDTH-1		8*WIDTH				4x 16-bit int	
+total length = 16*WIDTH (16 bytes)
+*/
+  
+SC_MODULE (matMul_2x2)
+{
+public:
+
+	tlm_utils::simple_target_socket<matMul_2x2, 32, tlm::tlm_base_protocol_types> port0;	
+  matMul_2x2(sc_module_name name): sc_module(name), port0("port0") 
+  {
+  	port0.register_b_transport(this, &matMul_2x2::b_transact);
+  }
+  
+	void b_transact(tlm::tlm_generic_payload &trans, sc_time &delay) 
+  {
+		unsigned char *data = trans.get_data_ptr();
+		
+		int i, j, k;
+		u32 valA = *(u32*) &data[0];
+		u32 valB = *(u32*) &data[4];
+		
+		u8* A = (u8*)&valA;
+		u8* B = (u8*)&valB;
+
+		u16 res[4] = {0, 0, 0, 0};
+
+		for(i=0;i<2;i++)
+    	for(j=0;j<2;j++) 
+        for(k=0;k<2;k++) 
+        	 res[i*2+j] +=  A[i*2+k] * B[k*2+j];
+        	
+		*(u64*)&data[8] = *(u64*) res;
+	  trans.set_response_status( tlm::TLM_OK_RESPONSE);
+  }
+  
+
+};
+
+
