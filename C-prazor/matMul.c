6d69c59b4417bdd27e721774c2a1d961663bd1cd
Christopher Watts <cjw218@cl.cam.ac.uk>
2017-03-14 20:41:37 +0000

    Add ability to provide matricies by command line input

--- C-prazor/matMul.c
+++ C-prazor/matMul.c
@@ -1,35 +1,54 @@
 /* C code to run on Prazor after plugging in the device */
 
+#ifdef USE_PRLIBC
+#include "prstdio.h"
+#else
 #include <stdio.h>
+#endif
+
 #define WRITE_BASE 0xE0002000
 #define READ_BASE 0xE0002010
+#define WRITE(offset, x) (((u32*)WRITE_BASE)[offset] = x)
+#define READ(offset) (((u32*)READ_BASE)[offset])
+
 #define u64 unsigned long long
 #define u32 unsigned int
-#define debug 
-int main()
-{
-	
-  u32* write = WRITE_BASE;
-  u32* read = READ_BASE;
-	u32 hRes, lRes;
-	u32 matA = 0x01020304;
-	u32 matB = 0x01000001;
 
-	for(int i=0;i<1;i++)
+int main(int argc, char* argv[])
+{
+	u32 iters = 1;
+	u32 matA, matB;
+
+	if (argc < 2 || argc > 3)
 	{
-		write[0] = matA;
-		write[2] = matB;		
-		lRes = read[0];
-		hRes = read[2];
-		
-		#ifdef debug
-			printf("\n\nstart\n");
-			printf("matrix A:\n%d %d\n%d %d\n", matA & 0xFF, matA>>8 & 0xFF, matA>>16 & 0xFF, matA>>24 & 0xFF); 
-			printf("matrix B:\n%d %d\n%d %d\n", matB & 0xFF, matB>>8 & 0xFF, matB>>16 & 0xFF, matB>>24 & 0xFF); 
-			printf("res = 0x%08x 0x%08x\n", hRes, lRes);
-			printf("res in matrix form:\n%d %d\n%d %d\n", lRes & 0xFFFF, lRes>>16 & 0xFFFF, hRes & 0xFFFF, hRes>>16 & 0xFFFF); 
-			printf("\n\n");
-		#endif
+		fprintf(stderr, "Usage: matMul <matrixA> <matrixB> [iterations]");
+		return 1;
 	}
+
+	if (argc == 3)
+	{
+		iters = argv[3];
+	}
+
+	sscanf(argv[1], "%x", &matA);
+	sscanf(argv[2], "%x", &matB);
+
+	u32 hRes, lRes;
+	u32 i;
+
+	printf("matrix A:\n%d %d\n%d %d\n", matA & 0xFF, matA>>8 & 0xFF, matA>>16 & 0xFF, matA>>24 & 0xFF); 
+	printf("matrix B:\n%d %d\n%d %d\n", matB & 0xFF, matB>>8 & 0xFF, matB>>16 & 0xFF, matB>>24 & 0xFF); 
+
+	for(i=0; i<iters; i++)
+	{
+		WRITE(0, matA);
+		WRITE(2, matB);		
+		lRes = READ(0);
+		hRes = READ(2);
+	}
+
+	printf("res = 0x%08x 0x%08x\n", hRes, lRes);
+	printf("res in matrix form:\n%d %d\n%d %d\n", lRes & 0xFFFF, lRes>>16 & 0xFFFF, hRes & 0xFFFF, hRes>>16 & 0xFFFF); 
+	printf("\n\n");
 }
 
