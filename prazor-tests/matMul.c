807eeccfe9403061eb7df813ec59fc7c9887c051
Christopher Watts <cjw218@cam.ac.uk>
2017-03-17 08:35:26 +0000

    Use zynq utils

--- prazor-tests/matMul.c
+++ prazor-tests/matMul.c
@@ -3,10 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WRITE_BASE 0xE0002000
-#define READ_BASE 0xE0002010
-#define WRITE(offset, x) (((u32*)WRITE_BASE)[offset] = x)
-#define READ(offset) (((u32*)READ_BASE)[offset])
+#include "zynq_utils.h"
+
+#define MEM_BASE 0xE0002000
+#define MATA    0x00
+#define MATB    0x08
+#define RES_LO  0x10
+#define RES_HI  0x18
+
+#define WRITE(offset, x) (*((u32*)(mem_base+offset)) = x)
+#define READ(offset) (*((u32*)(mem_base+offset)))
 
 #ifdef USE_ENERGYSHIM
 #include "energyshim.h"
@@ -18,15 +24,13 @@
 #define u32 unsigned int
 
 u32 matA, matB, iters;
+void* mem_base;
 
 int init(int argc, const char* argv[])
 {
-#ifndef USE_ENERGYSHIM
+#if !defined(USE_ENERGYSHIM) && !defined(__linux__)
 	printf("Turning on L1 and L2 caches\n");
-	__asm__("mov r0,#0x1000"); // Turn on L1 Cache (see Zynq TRM for further details.)
-	__asm__("orr r0,r0,#4");
-	__asm__("mcr  p15, 0, r0, c1, c0, 0"); //  (r0 = 0x1004)
-	((int*)0xF8F02100)[0] = 1; // Zynq: turn on L2 cache
+	zynq_enable_caches();
 #endif
 
 	printf("\n\n");
@@ -49,17 +53,31 @@ int init(int argc, const char* argv[])
 	matA = strtoul(argv[1], NULL, 16);
 	matB = strtoul(argv[2], NULL, 16);
 	
+	printf("Mapping memory\n");
+	mem_base = (void*) MEM_BASE;
+
+	printf("Clearing last results\n");
+	WRITE(MATA, 0);
+	WRITE(MATB, 0);
+
+	u32 res;
+	do {
+			res = READ(RES_LO) + READ(RES_HI);
+	} while (res != 0);
+
 	printf("input = 0x%08x 0x%08x\n", matA, matB);
 	printf("matrix A:\n%d %d\n%d %d\n", matA & 0xFF, matA>>8 & 0xFF, matA>>16 & 0xFF, matA>>24 & 0xFF); 
 	printf("matrix B:\n%d %d\n%d %d\n\n", matB & 0xFF, matB>>8 & 0xFF, matB>>16 & 0xFF, matB>>24 & 0xFF);
 
-	printf("iterations: %lu\n", iters); 
+	printf("iterations: %u\n", iters); 
 }
 
 int main(int argc, const char* argv[])
 {
 #ifndef USE_ENERGYSHIM
-	init(argc, argv);
+	int rc = init(argc, argv);
+	if (rc != 0)
+		return rc;
 #endif
 
 	u32 i;
@@ -67,10 +85,10 @@ int main(int argc, const char* argv[])
 
 	for(i=0; i<iters; i++)
 	{
-		WRITE(0, matA);
-		WRITE(2, matB);		
-		lRes = READ(0);
-		hRes = READ(2);
+		WRITE(MATA, matA);
+		WRITE(MATB, matB);
+		lRes = READ(RES_LO);
+		hRes = READ(RES_HI);
 	}
 
 #ifndef USE_ENERGYSHIM
