c1c2dd3706fd558279e7ee7ae95fcf7f64b461a7
Christopher Watts <cjw218@cam.ac.uk>
2017-04-12 18:23:47 +0100

    Move around utility files and add README

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simulated Matrix Multiplication
+
+In this folder you will find:
+
+* env.sh - A convenience script to set environment variables. Before building anytihng, call `source ./env.sh`
+* pure/ - A pure C implementation of the matrix multiplier. Build with `make`.
+* tlm/ - A SystemC + TLM 2.0 implementation. Build with `make`. Run with `make run`
+* prazor-tests/ - A test suite that runs the TLM implementation as an ARM binary on the Prazor virtual platform. Build with `make`. Run with `make run`.
+* utils/ - Utilities for energy monitoring and Zynq support.
+* vhls/ - The Prazor virtual platform code with plugin support.
+
+VHLS is already pre-built for Linux x86\_64, but to re-build VHLS, configure with:
+
+    ./configure --enable-plugins
+    make
+
+As long as the environment variables point to the right places in env.sh, VHLS will build with TLM POWER 3 and spEEDO.
+
+## Energy statistics
+
+To include energy usage, build TLM simulation models with `USE_ENERGYSHIM=1 make`. On real hardware with a compatible energy probe connected, build with `USE_ENERGYSHIM=1 NATIVE=1 make` and run with `USE_ENERGYSHIM=1 CURRENTPROBE=<hostname> make run` where `<hostname>` is the network name of the probe, such as `bognor.sm.cl.cam.ac.uk`
+
+For all energy statistics, energyshim must be compiled in. For energy statistics from a current probe, currentprobe-client must also be compiled in.
+
+Since the `NATIVE` environment variable only affects the compilation of the energyshim, as long as matMul.o is compiled with `USE_ENERGYSHIM=1`, the object file can be re-used for compilation on real hardware for direct comparison. 
---
+++ utils/zynq_utils.h
@@ -0,0 +1,91 @@
+#ifndef ZYNQ_UTILS_H
+#define ZYNQ_UTILS_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+#define PAGE_SIZE 4096UL
+#define PAGE_MASK (PAGE_SIZE - 1)
+
+static void* map_page(off_t target)
+{
+	int fd = -1;
+	unsigned int page = 0;
+	void *map_base = 0;
+
+	fd = open("/dev/mem", O_RDWR | O_SYNC);
+	if (fd < 0)
+	{
+		fprintf(stderr, "lowlevel_accessor failed %s:%i Am I running with root privelege?\n", __FILE__, __LINE__);
+		return 0;
+	}
+
+	if (map_base) // check page is still the correct one                                                                                                                                                       
+	{
+		unsigned int page_primed = target & ~PAGE_MASK;
+		if (page != page_primed)
+		{
+			fprintf(stderr, "lowlevel_accessor failed %s:%i new page requested : %x cf %x\n", __FILE__, __LINE__, page_primed, page);
+			return 0;
+		}
+	}
+	else
+	{
+		// Map one page
+		page = target & ~PAGE_MASK;
+		map_base = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, page);
+		if (map_base == (void *) -1)
+		{
+			fprintf(stderr, "lowlevel_accessor failed %s:%i failed to map page\n", __FILE__, __LINE__);
+			return 0;
+		}
+	}
+	
+	close(fd);
+
+	return map_base;
+}
+
+static void unmap_page(void* page)
+{
+	if (page > 0)
+		munmap(page, PAGE_SIZE);
+}
+
+static void _zynq_gic_ints_enable(int enable)
+{
+	off_t target = 0xF8F01000; // GIC master interrupt control register
+	void *map_base = map_page(target);
+	void *virt_addr = map_base + (target & PAGE_MASK);
+	
+	((unsigned long *) virt_addr)[0] = enable ? 1 : 0;
+	
+	unmap_page(map_base);
+}
+
+static void zynq_disable_interrupts()
+{
+	_zynq_gic_ints_enable(0);
+}
+
+static void zynq_enable_interrupts()
+{
+	_zynq_gic_ints_enable(1);
+}
+
+static void zynq_enable_caches()
+{
+	// L1 Cache (see Zynq TRM for further details)
+	__asm__("mov r0, #0x1000");
+	__asm__("orr r0, r0, #4");
+	__asm__("mcr p15, 0, r0, c1, c0, 0");
+	
+	// L2 Cache
+	off_t l2target = 0xF8F02100;
+	((unsigned long *) l2target)[0] = 1;
+}
+
+#endif
